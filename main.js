1fcba9af56478965a76c3b29a549d1dd7fea169d
Rsabaric <rosie.sabaric@gmail.com>
2018-09-01 04:06:18 +0000

    trying to get image to respond to console

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-fs
+/*WATS 3020 Final*/
+/* Rosie Sabaric */
+
+/* image event listener */
+document.getElementById("theIm").addEventListener("click", displayModal);
+
+function displayModal() {
+ console.log("peanut");  //* document.getElementById("demo").innerHTML = Date();
+}
+
+
+
+
+
+
+
+
+
+
+
+
