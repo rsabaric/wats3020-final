c6b8a0fb0d53ed8dfd5e09a56ff67f89d54b811a
Rsabaric <rosie.sabaric@gmail.com>
2018-09-02 00:32:52 +0000

    Adding conditionals

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ var yesButton= document.querySelector('#Yes');
 var noButton= document.querySelector('#No');
 var h3Text = document.querySelector('h3');
 var pDirections = document.querySelector('#Directions');
-console.log(pDirections);
+var fundsButton= document.getElementById("Go");
+var totalValue;
+var text;
+//console.log(pDirections);
 var i;
 for (i = 0; i < images.length; i++) {
     let click = i;
@@ -44,7 +47,7 @@ function showGalleryYes(){
     galleryYes.style.display = "unset";
     galleryNo.style.display = "none";
     h3Text.innerHTML= 'I thought so!';
-    pDirections.innerHTML= 'Browse through the image gallery and select the image to begin planning!';
+    pDirections.innerHTML= 'Browse through the image gallery and select the image to get some ideas!';
 }
 
 function showGalleryNo(){
@@ -54,6 +57,23 @@ function showGalleryNo(){
     pDirections.innerHTML= 'This is what you are looking forward to...';
 }
 
+fundsButton.addEventListener("click", fundsCalc);
+function fundsCalc (){
+    totalValue = document.getElementById("vacationfund").value;
+    console.log(totalValue);
+    if (isNaN(totalValue)){
+        text = "You need to enter a valid number! I cannot help you if I don't know how much you have or do not have. Thank You!" 
+    } else if (totalValue<0){
+        text = "You are in debt, so please pay off your credit card.";
+    } else if (totalValue>=0 && totalValue<200){
+        text = "You do not have quite enough for a big vacation, but your parents or grandparents would be happy to see you :)";
+    } else if (totalValue>=200 && totalValue<1000){
+        text = "You may consider somewhere local or in the off season.";
+    } else {
+        text = "You can go anywhere!";
+    }
+    document.getElementById("fund").innerHTML = text;
+}
 
 
 
